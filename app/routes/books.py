1b34a0c582d21207853f28cca97124da625411d0
zorua <ari.pokemon.storm@gmail.com>
2026-04-15 12:07:46 +0200

    :b

--- app/routes/books.py
+++ app/routes/books.py
@@ -1,15 +1,40 @@
 from fastapi import APIRouter
 router = APIRouter()
 
+books = [{"id": 1, "title": "Fooled by Randomness", "author": "Nassim Nicholas Taleb", "year": 2001},
+         {"id": 2, "title": "Hunger Games",
+             "author": "Suzanne Collins", "year": 2008},
+         {"id": 3, "title": "The Great Gatsby",
+             "author": "F. Scott Fitzgerald", "year": 1925},
+         {"id": 4, "title": "The Wave", "author": "Todd Strasser", "year": 1981},
+         {"id": 5, "title": "Warrior Cats", "author": "Erin Hunter", "year": 2003},
+         {"id": 6, "title": "Wings of Fire", "author": "Tui T. Sutherland", "year": 2012}]
+
 
 @router.get("/books")
 def read_books():
     return books
 
 
-books = [{"title": "Fooled by Randomness", "author": "Nassim Nicholas Taleb", "year": 2001},
-         {"title": "Hunger Games", "author": "Suzanne Collins", "year": 2008},
-         {"title": "The Great Gatsby", "author": "F. Scott Fitzgerald", "year": 1925},
-         {"title": "The Wave", "author": "Todd Strasser", "year": 1981},
-         {"title": "Warrior Cats", "author": "Erin Hunter", "year": 2003},
-         {"title": "Wings of Fire", "author": "Tui T. Sutherland", "year": 2012}]
+@router.post("/books")
+def add_book(book: dict):
+    books.append(book)
+    return {"message": "book added successfully 𐔌*ˊᵕˋ*𐦯"}
+
+
+@router.put("/books/{book_id}")
+def update_book(book_id: int, book: dict):
+    for i, b in enumerate(books):
+        if b["id"] == book_id:
+            books[i] = book
+            return {"message": "book updated successfully 𐔌*ˊᵕˋ*𐦯"}
+    return {"message": "book not found (ㅠ﹏ㅠ)"}
+
+
+@router.delete("/books/{book_id}")
+def delete_book(book_id: int):
+    for i, b in enumerate(books):
+        if b["id"] == book_id:
+            del books[i]
+            return {"message": "book deleted successfully 𐔌*ˊᵕˋ*𐦯"}
+    return {"message": "book not found (ㅠ﹏ㅠ)"}
